474bc91a819e40d0518c30e9e8a6dcb038c0f81b
CristiO84 <cristi.olaru@gmail.com>
2025-07-31 15:08:23 +0300

    Create WalletChanger.jsx

---
+++ src/components/WalletChanger.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import WalletChanger from './components/WalletChanger'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Qubetics Wallet Switch</h1>
+      <WalletChanger />
+    </div>
+  )
+}
+
+export default App
